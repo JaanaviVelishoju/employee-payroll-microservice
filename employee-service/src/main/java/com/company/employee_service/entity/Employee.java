c57b3e1db87f27c274294586eaab974c252e54e7
Jaanavi <jaanavi.rjr@gmail.com>
2025-12-18 16:56:02 +0530

    Employee and service is created

--- employee-service/src/main/java/com/company/employee_service/entity/Employee.java
+++ employee-service/src/main/java/com/company/employee_service/entity/Employee.java
@@ -1,17 +1,28 @@
 package com.company.employee_service.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+@Entity
+@Table(name="employee")
 @Data
 public class Employee {
 
     @Id
     @GeneratedValue
     private long id;
+
+    @NotBlank
     private String name;
+
+    @Email
     private String email;
+
     private String department;
     private Double salary;
 }
